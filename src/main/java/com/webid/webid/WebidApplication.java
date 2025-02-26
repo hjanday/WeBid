dfa7624d0bb4a96b3d1f480e465e4cd9f69e85ef
hjanday <jandaypreet@gmail.com>
2025-02-25 21:23:32 -0500

    remove security for now

--- src/main/java/com/webid/webid/WebidApplication.java
+++ src/main/java/com/webid/webid/WebidApplication.java
@@ -3,7 +3,7 @@ package com.webid.webid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class WebidApplication {
