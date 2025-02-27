bf3640bbf665e0bfad35a56655ed4d3a9db0668c
hjanday <jandaypreet@gmail.com>
2025-02-27 12:04:18 -0500

    resolve test containers compilation issue

--- src/test/java/com/webid/webid/TestWebidApplication.java
+++ src/test/java/com/webid/webid/TestWebidApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 public class TestWebidApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(WebidApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(WebidApplication::main).run(args);
+		//.with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
