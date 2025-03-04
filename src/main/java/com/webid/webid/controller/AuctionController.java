1bff1a3020992dfebb40386bc79072f7e1b402d7
Chaplik-Gabriel <gabrielchaplik@gmail.com>
2025-03-04 13:11:19 -0500

    Added api mapping to Auction Controller

--- src/main/java/com/webid/webid/controller/AuctionController.java
+++ src/main/java/com/webid/webid/controller/AuctionController.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.webid.webid.model.Auction;
 import com.webid.webid.repository.AuctionRepository;
 import com.webid.webid.service.AuctionService;
-
+@RestController
+@RequestMapping("/api/Auction")
 public class AuctionController {
     private final AuctionService as;
     public AuctionController(AuctionService as) {
         
         this.as = as;
     }
+    @PostMapping("/Search")
     public ArrayList<Auction> findItem(String itemName){
     	try {
     		return this.as.search(itemName);
@@ -23,6 +28,8 @@ public class AuctionController {
     	}
     	
     }
+    @PostMapping("/Select")
+
     public Auction selectItem(ArrayList<Auction> items, Long id) {
     	
         return this.as.select(items,id);
