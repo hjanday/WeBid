c604df651a7f22502fd231ba02d7104e17bd81bf
hjanday <jandaypreet@gmail.com>
2025-03-30 14:44:18 -0400

    update logout and update logout.js

--- src/main/java/com/webid/webid/controller/AuthController.java
+++ src/main/java/com/webid/webid/controller/AuthController.java
@@ -53,8 +53,7 @@ public class AuthController {
 		  cookie.setPath("/"); // cookie is available to all paths in your domain
 		  cookie.setHttpOnly(true); // prevents JavaScript access to the cookie
 		  cookie.setSecure(true); // ensure this is true if you use HTTPS
-		  cookie.setMaxAge(2700); // ttl in seconds //45 minutes
-		  
+		  cookie.setMaxAge((int)jwtService.getJwtTTL()); // ttl in seconds
 
 		LoginResponse login_response = new LoginResponse(jwtToken, jwtService.getJwtTTL(), authenticatedUser.getRoles());
 		// add cookie to servlet response
