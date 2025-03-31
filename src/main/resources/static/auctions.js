df83456a69580d3127de941ebe0556686e5d1d12
Ricky Mach <machricky2002@gmail.com>
2025-03-31 14:56:42 -0400

    Revert "dutch buttons"
    This reverts commit abb4c029d5fcb82f5b6810f5b06532184914728a.

--- src/main/resources/static/auctions.js
+++ src/main/resources/static/auctions.js
@@ -98,6 +98,15 @@ document
     alert("Dutch auction decremented successfully!");
     })
     .then((data) => {
+        // If the response was successful, handle the bid data
+        if (data && data.id) {
+        // Assuming 'data' is a Bid object returned by the backend
+        alert(
+            `Bid placed successfully! Server response: ${JSON.stringify(
+            data
+            )}`
+        );
+
         // Update UI with new auction information
         const newCurrentBid =
         data.currentBid != null && data.currentBid !== ""
@@ -105,11 +114,14 @@ document
             : "No Bid Yet";
         document.getElementById("faCurrentPrice").textContent =
             "Current Price: " + newCurrentBid;
+        document.getElementById("faHighestBidder").textContent =
+            "Highest Bidder: " + (auc.currentBidderID || "Unknown");
         }
     })
     .catch(error => {
         console.error("Request failed:", error);
     });
+});
 
 // place bid
 document
@@ -259,9 +271,7 @@ document
     .then((response) => {
         console.log(response);
         if (!response.ok) {
-            return response.text().then(text => {
-                throw new Error(text);  // Extract error message from backend
-              });
+            throw new Error(response);
         }
         alert("Dutch auction completed successfully!");
         window.location.href = `/paynow?auctionId=${selectedAuction.id}`;
